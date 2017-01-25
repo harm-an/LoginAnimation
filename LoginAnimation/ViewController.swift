be2e745040e0e74a8241137f017d62b23e00b5d1
Mitesh <mitesh@AmaAdmins-MacBook-Pro-2.local>
2017-01-25 22:03:43 +0530

    Initial Commit

---
+++ LoginAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginAnimation
+//
+//  Created by Mitesh on 25/01/17.
+//  Copyright © 2017 Mitesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
